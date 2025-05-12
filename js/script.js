29c0f8f37f06affe2d0876dbd32faceea6df8d1a
LuxScintilla <catherina.ea@gmail.com>
2025-05-12 14:51:35 +0100

    added 3 sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Toggle hamburger mobile menu
+
 document.addEventListener("DOMContentLoaded", function () {
   const mobileToggle = document.querySelector(".mobile-menu-toggle");
   const mobileMenu = document.querySelector(".mobile-menu-items");
@@ -5,4 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
   mobileToggle.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
+});
+
+// Change navbar background on scroll
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  }
+  if (window.scrollY === 0) {
+    navbar.classList.remove("navbar-scroll");
+  }
 });
